dd7a0deb29b50a0e3920ce5fed6486b3cd55ce65
Srimali <jasrimalimadurangani@gmail.com>
2025-03-09 09:34:55 +0530

    Create Bill.js file 🆕

--- BACKEND/controllers/doctorController.js
+++ BACKEND/controllers/doctorController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const Appointment = require("../models/Appointment");
 const Prescription = require("../models/Prescription");
