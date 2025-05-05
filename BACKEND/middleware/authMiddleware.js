798290871c9e6879f07a8cf2f2e8f6f373cf89de
Srimali <jasrimalimadurangani@gmail.com>
2025-05-05 20:51:22 +0530

    Create clinicalMiddleware file

--- BACKEND/middleware/authMiddleware.js
+++ BACKEND/middleware/authMiddleware.js
@@ -1,22 +1,24 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (roles) => (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) return res.status(403).json({ message: 'Access denied' });
 
-  if (!token) {
-    return res.status(401).json({ error: 'Access denied. No token provided.' });
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!roles.includes(decoded.role)) {
+            return res.status(403).json({ message: 'Access denied' });
+        }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (!roles.includes(decoded.role)) {
-      return res.status(403).json({ error: 'Access denied. Invalid role.' });
+        req.user = {
+            id: decoded.userId,
+            role: decoded.role,
+            fullName: decoded.name,
+        };
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Invalid or expired token' });
     }
-    req.user = decoded; // Attach user details to the request
-    next();
-  } catch (error) {
-    res.status(400).json({ error: 'Invalid token.' });
-  }
 };
 
 module.exports = authMiddleware;
