9b38a0e803899485494249f216251ef81a7cbc6d
JayaniPerera27 <pererajayani27@gmail.com>
2025-05-09 12:05:38 +0530

    just change

--- BACKEND/middleware/authMiddleware.js
+++ BACKEND/middleware/authMiddleware.js
@@ -14,6 +14,7 @@ const authMiddleware = (roles) => (req, res, next) => {
             id: decoded.userId,
             role: decoded.role,
             fullName: decoded.name,
+            
         };
         next();
     } catch (err) {
